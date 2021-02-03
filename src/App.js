3eeaf0f07a715d7cc955b9e84fd01b118e7af65b
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-03 22:28:39 +0600

    add fragment for load time issu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Fragment } from 'react';
 
 function App() {
    return (
-      <div className="App">
 
-      </div>
+      <Fragment>
+         <div className="App">
+
+         </div>
+      </Fragment>
+
+
    );
 }
 
