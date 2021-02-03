eef069cbf208ac1d5fd3a491bc879d6deada4963
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-03 23:30:46 +0600

    import user context

--- src/components/Delete.js
+++ src/components/Delete.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Fragment } from 'react'
 import { useParams } from 'react-router-dom'
+import { UserContext } from './UserContext/UserContext'
 
 const Delete = () => {
+   const { users, setUsers } = useContext(UserContext)
    const { id } = useParams()
    return (
       <Fragment>
