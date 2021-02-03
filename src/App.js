134a76802743935bad78a7cb667750d3a21d37da
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-03 22:36:08 +0600

    set our route and create home components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Fragment } from 'react';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import Home from './components/Home'
 
 function App() {
    return (
 
       <Fragment>
          <div className="App">
-
+            <Router>
+               <Link to='/'></Link>
+               <Route exact path='/' component={Home}></Route>
+            </Router>
          </div>
       </Fragment>
 
