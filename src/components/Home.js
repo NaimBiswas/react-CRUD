aac8d19a1c0fb38622664ff440f62ed5d9714711
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-06 21:35:17 +0600

    add bootstrap mobal

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
-import React, { useContext } from 'react'
-import { Button, Table } from 'react-bootstrap'
+import React, { useContext, useState } from 'react'
+import { Button, Modal, Table } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
 import { UserContext } from './UserContext/UserContext';
 
 const Home = () => {
    const [users, setUsers] = useContext(UserContext);
+   const [show, setShow] = useState(false);
 
+   const handleClose = () => setShow(false);
+   const handleShow = () => setShow(true);
    return (
       <div className='container'>
          <Button variant='outline-success mt-5 mb-2'>Add New User</Button>
@@ -29,9 +32,9 @@ const Home = () => {
                            <td> {user.position} </td>
                            <td> {user.salary} </td>
                            <td>
-                              <Link to={'/delete/' + user.id}>
-                                 <Button variant='outline-danger ml-1 btn-md'>Remove</Button>
-                              </Link>
+
+                              <Button onClick={handleShow} variant='outline-danger ml-1 btn-md'>Remove</Button>
+
                               <Button variant='outline-info ml-1 btn-md'>Show</Button>
                               <Button variant='outline-warning  ml-1 btn-md'>Edit</Button>
                            </td>
@@ -41,6 +44,25 @@ const Home = () => {
                </tbody>
             </Table>
          </div>
+         {/* Modal Here  */}
+         {/* <Button variant="primary" >
+            Launch demo modal
+      </Button> */}
+
+         <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+               <Modal.Title>Modal heading</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Hei Are You Sure! Delete Him, If not just click on No we will save him</Modal.Body>
+            <Modal.Footer>
+               <Button variant="secondary" onClick={handleClose}>
+                  No! I Don't
+          </Button>
+               <Button variant="danger" onClick={handleClose}>
+                  Yes! Delete Him
+          </Button>
+            </Modal.Footer>
+         </Modal>
       </div >
    )
 }
